995a1b0e22396ee98628296ab8a6178257858217
Daniel Santos <daniel.martins@lumedigital.com.br>
2020-04-02 02:19:00 -0300

    Issue #7: Added initial configuration to swagger(open-api)

--- dj_stone_profit_sharing_api/config/urls.py
+++ dj_stone_profit_sharing_api/config/urls.py
@@ -1,9 +1,13 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
@@ -14,16 +18,46 @@ urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    path("users/", include("dj_stone_profit_sharing_api.users.urls", namespace="users")),
+    path(
+        "users/", include("dj_stone_profit_sharing_api.users.urls", namespace="users")
+    ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Profit Calculation API",
+        default_version="v1",
+        description="Calcute profit distribution among the employee",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    url="",
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 # API URLS
 urlpatterns += [
     # API base url
     path("api/", include("config.api_router")),
     # DRF auth token
     path("auth-token/", obtain_auth_token),
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
 
 if settings.DEBUG:
